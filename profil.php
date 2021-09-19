482442841efa31d9883b73a48b81dad5c833f1da
Muhfrrazi <mfachrurrazi41@gmail.com>
2021-09-20 06:36:56 +0800

    menghapus link style.css

--- profil.php
+++ profil.php
@@ -20,7 +20,6 @@ if ($_SESSION['status_login'] != true) {
     <meta charset="utf-8">
     <meta name="viewport" content="width_device-width, initial-scale=1">
     <title>Peduli Covid - Profil</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
     <link href="https://fonts.googleapis.com/css2?family=Quicksand&display=swap" rel="stylesheet">
 </head>
 
